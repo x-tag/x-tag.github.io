4b17289e09071caa80f3289d8100ba03bb8a419a
Daniel Buchner <danieljb2@gmail.com>
2016-01-05 08:50:03 -0800

    new chat integration

--- js/site.js
+++ js/site.js
@@ -26,34 +26,3 @@ xtag.register('x-clock', {
     }
   }
 });
-
-(function(){
-
-var ready = false;
-
-Object.defineProperty(Smooch, 'ready', {
-  set: function(val){
-    ready = !!val;
-    if (ready) {
-      var chatElement = document.getElementById('sk-container');
-      var chatPanel = document.getElementById('community_chat');
-      chatPanel.appendChild(chatElement);
-    }
-  },
-  get: function(){
-    return ready;
-  }
-});
-
-Smooch.init({
-  appToken: '3lif3pcvgkts4b79n30phmiry',
-  customText: {
-    headerText: 'Let\'s talk X-Tag & Web Components!',
-    inputPlaceholder: 'Type a message...',
-    sendButtonText: 'Send',
-    introText: 'This is the beginning of your conversation.<br/> Fire away!',
-    settingsText: 'You can leave us your email so that we can get back to you this way.'
-  }
-});
-
-})();
